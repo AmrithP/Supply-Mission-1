a676b6b46e66030503f101f569ad595b474b0af5
AmrithP <67651072+AmrithP@users.noreply.github.com>
2020-07-14 15:25:02 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+//variables are defined
+
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground
 var background1, backgroundIMG;
 var text1, text1Image, text2, text2Image;
+
+//niknames are created
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -10,6 +14,9 @@ const Body = Matter.Body;
 
 function preload()
 {
+
+	//images are being loded to corresponding var name
+
 	helicopterIMG=loadImage("helicopter.png");
 	packageIMG=loadImage("package.png");
 	backgroundIMG = loadImage("AcZacvL.jpg");
@@ -20,11 +27,20 @@ function preload()
 
 function setup() {
 
+	//canvas is formed
+
 	createCanvas(800, 700);
+
+	//placement of the rectangle
+
 	rectMode(CENTER);
+
+	//background is created and image is added to background
 	
 	background1 = createSprite(400,350,800,700);
 	background1.addImage(backgroundIMG);
+
+	//sprites are created and images are being added to them
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
@@ -37,6 +53,8 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255);
 
+	//texts show reader what to do
+
 	text1 = createSprite(660,140,50,50);
 	text1.addImage(text1Image);
 	text1.scale = 0.2;
@@ -48,36 +66,47 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
+	//supplies sprite is creates and added to world
+
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
 	World.add(world, packageBody);
 	
-
 	//Create a Ground
+
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
 	Engine.run(engine);
-
-	
   
 }
 
 
 function draw() {
+
   rectMode(CENTER);
+
+  //background is craeted
+
   background(0);
 
+  //packagesprite and body have same x position
 
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-  drawSprites();
+
+  //sprites are drawn
+
+  drawSprites(); 
+
   console.log(packageBody);
  
 }
 
 function keyPressed() {
+
+	//supplies fall to gorund with down arrow
+
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
 	Matter.Body.setStatic(packageBody, false);
   }
 }
